468224b156970ec040b50a46eea74ab3366ca97d
Pankaj Yadav <91666271+pyadav123@users.noreply.github.com>
2023-01-13 12:56:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let section=document.querySelector("section");
+fetch("https://api.github.com/users")
+.then(res=>{
+      res.json()
+      .then(data=>{
+        data.map(value=>{
+            let {login,avatar_url,html_url}=value;
+            section.innerHTML+=`
+            <div>
+            <img src=${avatar_url} alt=${login} width=200px">
+            <h1>${login}</h1>
+            <a href=${html_url} target="_blank">Goto Github Profile</a>
+            </div>`;
+        });
+      })
+      .catch(err=>console.log(err));
+})
+.catch(err=>console.log(err))
+.finally(()=>{
+    console.log("server response");
+});
